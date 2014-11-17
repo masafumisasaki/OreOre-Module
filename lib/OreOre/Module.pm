fc703e8d8abf836bcff4a806ae201c82407b7d4a
masafumisasaki <yokotesimin2058@gmail.com>
2014-11-17 20:23:08 +0900

    initial commit

---
+++ lib/OreOre/Module.pm
@@ -0,0 +1,39 @@
+package OreOre::Module;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+OreOre::Module - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use OreOre::Module;
+
+=head1 DESCRIPTION
+
+OreOre::Module is
+
+=head1 AUTHOR
+
+Masafumi Sasaki E<lt>yokotesimin2058@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Masafumi Sasaki
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
